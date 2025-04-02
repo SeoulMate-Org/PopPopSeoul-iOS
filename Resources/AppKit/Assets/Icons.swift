5dd203b96f3d656520c1256b9adaa4cb792ba3dd
SuniDev <sun-918@naver.com>
2025-04-02 15:27:43 +0900

    feat: 디자인 시스템 기반 Icon Assets 설정
    - Figma 디자인 시스템을 바탕으로 Icon Assets 구성
    - SwiftGen과 연동하여 코드 기반 색상 접근 준비 완료

--- Resources/AppKit/Assets/Icons.swift
+++ Resources/AppKit/Assets/Icons.swift
@@ -13,6 +13,8 @@
 #endif
 
 // Deprecated typealiases
+@available(*, deprecated, renamed: "ImageAsset.Image", message: "This typealias will be removed in SwiftGen 7.0")
+public typealias AssetImageTypeAlias = ImageAsset.Image
 
 // swiftlint:disable superfluous_disable_command file_length implicit_return
 
@@ -20,10 +22,111 @@
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 public enum Icon {
+  public static let arrowLeftLine = ImageAsset(name: "arrow_left_line")
+  public static let arrowRightLine = ImageAsset(name: "arrow_right_line")
+  public static let checkLine = ImageAsset(name: "check_line")
+  public static let deleteBtnLine = ImageAsset(name: "delete_btn_line")
+  public static let embassyLine = ImageAsset(name: "embassy_line")
+  public static let emptyLine = ImageAsset(name: "empty_line")
+  public static let heartFill = ImageAsset(name: "heart_fill")
+  public static let heartLine = ImageAsset(name: "heart_line")
+  public static let hospitalLine = ImageAsset(name: "hospital_line")
+  public static let locationLine = ImageAsset(name: "location_line")
+  public static let mapFill = ImageAsset(name: "map_fill")
+  public static let mapLine = ImageAsset(name: "map_line")
+  public static let searchLine = ImageAsset(name: "search_line")
+  public static let shareLine = ImageAsset(name: "share_line")
+  public static let themFill = ImageAsset(name: "them_fill")
+  public static let themeLine = ImageAsset(name: "theme_line")
+  public static let toiletLine = ImageAsset(name: "toilet_line")
+  public static let warningLine = ImageAsset(name: "warning_line")
+  public static let wifiLine = ImageAsset(name: "wifi_line")
+  public static let xLine = ImageAsset(name: "x_line")
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
 // MARK: - Implementation Details
+
+public struct ImageAsset {
+  public fileprivate(set) var name: String
+
+  #if os(macOS)
+  public typealias Image = NSImage
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  public typealias Image = UIImage
+  #endif
+
+  @available(iOS 8.0, tvOS 9.0, watchOS 2.0, macOS 10.7, *)
+  public var image: Image {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    let image = Image(named: name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    let name = NSImage.Name(self.name)
+    let image = (bundle == .main) ? NSImage(named: name) : bundle.image(forResource: name)
+    #elseif os(watchOS)
+    let image = Image(named: name)
+    #endif
+    guard let result = image else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 8.0, tvOS 9.0, *)
+  public func image(compatibleWith traitCollection: UITraitCollection) -> Image {
+    let bundle = BundleToken.bundle
+    guard let result = Image(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+  #endif
+
+  #if canImport(SwiftUI)
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+  public var swiftUIImage: SwiftUI.Image {
+    SwiftUI.Image(asset: self)
+  }
+  #endif
+}
+
+public extension ImageAsset.Image {
+  @available(iOS 8.0, tvOS 9.0, watchOS 2.0, *)
+  @available(macOS, deprecated,
+    message: "This initializer is unsafe on macOS, please use the ImageAsset.image property")
+  convenience init?(asset: ImageAsset) {
+    #if os(iOS) || os(tvOS)
+    let bundle = BundleToken.bundle
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSImage.Name(asset.name))
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+#if canImport(SwiftUI)
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+public extension SwiftUI.Image {
+  init(asset: ImageAsset) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle)
+  }
+
+  init(asset: ImageAsset, label: Text) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle, label: label)
+  }
+
+  init(decorative asset: ImageAsset) {
+    let bundle = BundleToken.bundle
+    self.init(decorative: asset.name, bundle: bundle)
+  }
+}
+#endif
 
 // swiftlint:disable convenience_type
 private final class BundleToken {
