4865ce17a7436ff768daafa2e0364ec3fb976f72
SuniDev <sun-918@naver.com>
2025-04-08 03:04:20 +0900

    feat: 메인 탭 UI 개발

--- Sources/AppKit/Features/IntroFeature/IntroView.swift
+++
@@ -1,6 +0,0 @@
-//
-//  IntroView.swift
-//  SeoulMateKit
-//
-//  Created by suni on 4/5/25.
-//
--- Sources/AppKit/Features/MainTabFeature/MainTabView.swift
+++ Sources/AppKit/Features/MainTabFeature/MainTabView.swift
@@ -17,67 +17,59 @@ public struct MainTabView: View {
   }
 
   public var body: some View {
-    HStack {
-      themeTab
-      mapTab
-      likesTab
+    VStack {
+      Spacer()
+      HStack {
+        Spacer(minLength: 20)
+        
+        tabItem(icon: Assets.Icons.themeLine.swiftUIImage, label: "테마추천") {
+          store.send(.themeTapped)
+        }
+        
+        tabItem(icon: Assets.Icons.mapLine.swiftUIImage, label: "지도") {
+          store.send(.mapTapped)
+        }
+        
+        tabItem(icon: Assets.Icons.heartLine.swiftUIImage, label: "좋아요") {
+          store.send(.likesTapped)
+        }
+      }
+      .frame(height: 58)
+      .background(Colors.white.swiftUIColor)
+      .modifier(ShadowModifier(
+        shadow: AppShadow(
+          color: Colors.trueBlack.swiftUIColor.opacity(0.08),
+          x: 0,
+          y: -4,
+          blur: 12,
+          spread: 0
+        )
+      ))
     }
-    .frame(height: 58)
-    .background(Colors.white.swiftUIColor)
-    .modifier(ShadowModifier(
-      shadow: AppShadow(
-        color: Colors.trueBlack.swiftUIColor.opacity(0.08),
-        x: 0,
-        y: -4,
-        blur: 12,
-        spread: 0
-      )
-    ))
   }
   
-  var themeTab: some View {
-    Button {
-      store.send(.themeTapped)
-    } label: {
-      Assets.Icons.themeLine.swiftUIImage
+  @ViewBuilder
+  func tabItem(icon: Image, label: String, action: @escaping () -> Void) -> some View {
+    VStack {
+      Button(action: action) {
+        VStack(spacing: 4) {
+          icon
+            .resizable()
+            .frame(width: 24, height: 24)
+
+          Text(label)
+            .font(.system(size: 12))
+            .foregroundColor(.black)
+        }
+      }
+      .buttonStyle(PlainButtonStyle())
+      .contentShape(Rectangle())
     }
-    .frame(width: 30, height: 30)
-  }
-  
-  var mapTab: some View {
-    Button {
-      store.send(.mapTapped)
-    } label: {
-      Assets.Icons.mapLine.swiftUIImage
-    }
-    .frame(width: 30, height: 30)
-  }
-  
-  var likesTab: some View {
-    Button {
-      store.send(.likesTapped)
-    } label: {
-      Assets.Icons.heartLine.swiftUIImage
-    }
-    .frame(width: 30, height: 30)
+    .frame(maxWidth: .infinity)
   }
 }
 
 // MARK: Preview
-
-//#if DEBUG
-//  struct MainTabView_Previews: PreviewProvider {
-//    static var previews: some View {
-//      MainTabView(
-//        store: Store<MainTabFeature.State, MainTabFeature.Action>(
-//          initialState: .init(),
-//          reducer: { MainTabFeature() }
-//        )
-//      )
-//    }
-//  }
-//#endif
-
 #Preview {
   MainTabView(
     store: Store<MainTabFeature.State, MainTabFeature.Action>(
